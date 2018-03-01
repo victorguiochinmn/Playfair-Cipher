51b263dd1f11c4c4a3f8bec757f25c3e2eccca93
José Alonso <jose.zavalapo@udlap.mx>
2018-02-28 23:02:50 -0600

    trigramas
    crea trigramas, esta version ya realiza la primera de las tres partes del trabajo y el principio de la encriptación

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
+
     static ArrayList<String> newpass = new ArrayList<String>();
     static ArrayList<String> listpass = new ArrayList<String>();
 
-    String[][] floor1 = new String [4][4];
-    String[][] floor2 = new String [4][4];
-    String[][] floor3 = new String [4][4];
-    String[][] floor4 = new String [4][4];
+    //Matrices de cada piso
+    static String[][] floor1 = new String [4][4];
+    static String[][] floor2 = new String [4][4];
+    static String[][] floor3 = new String [4][4];
+    static String[][] floor4 = new String [4][4];
 
+    //Revisamos la contrasena para que salga sin letras repetidas
     public static ArrayList<String> checkPass (String password) {
         newpass.add(String.valueOf(password.charAt(0)));
         int k;
@@ -24,14 +26,94 @@ public class Main {
                 newpass.add(String.valueOf(password.charAt(i)));
             }
         }
-        System.out.println(newpass);
         return newpass;
     }
 
-    public static ArrayList<String> preLlenado (ArrayList<String> password) {
-        for (int i= 0 ; i < password.size();i++) {
+    //Llenamos todas la matrices con la nueva contrasena y los caracteres restantes
+    public static void llenarMatrices(ArrayList<String> passwd) {
+
+        //COLOCAMOS PRIMERO LA CONTRASENA Y RESPETAMOS EL ORDEN DE NUMEROS > LETRAS > SIMBOLOS
+        for(int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (!(passwd.isEmpty())) {
+                    floor1[i][j] = passwd.get(0);
+                    passwd.remove(0);
+                } else if (passwd.isEmpty() && i < 4 && j < 4) {
+                    floor1[i][j] = listpass.get(0);
+                    listpass.remove(0);
+                }
+            }
+        }
+
+        for(int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (!(passwd.isEmpty())) {
+                    floor2[i][j] = passwd.get(0);
+                    passwd.remove(0);
+                } else if (passwd.isEmpty() && i < 4 && j < 4) {
+                    floor2[i][j] = listpass.get(0);
+                    listpass.remove(0);
+                }
+            }
+        }
+
+        for(int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (!(passwd.isEmpty())) {
+                    floor3[i][j] = passwd.get(0);
+                    passwd.remove(0);
+                } else if (passwd.isEmpty() && i < 4 && j < 4) {
+                    floor3[i][j] = listpass.get(0);
+                    listpass.remove(0);
+                }
+            }
+        }
+
+        for(int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (!(passwd.isEmpty())) {
+                    floor4[i][j] = passwd.get(0);
+                    passwd.remove(0);
+                } else if (passwd.isEmpty() && i < 4 && j < 4) {
+                    floor4[i][j] = listpass.get(0);
+                    listpass.remove(0);
+                }
+            }
+        }
+
+        //IMPRIMIMOS LAS MATRICES PARA REVISAR SU LLENADO
+        for (int l = 0; l < 4; l++) {
+            for (int m = 0; m < 4; m++) {
+                System.out.println(floor1[l][m]);
+            }
+        }
+
+        for (int l = 0; l < 4; l++) {
+            for (int m = 0; m < 4; m++) {
+                System.out.println(floor2[l][m]);
+            }
+        }
+
+        for (int l = 0; l < 4; l++) {
+            for (int m = 0; m < 4; m++) {
+                System.out.println(floor3[l][m]);
+            }
+        }
+
+        for (int l = 0; l < 4; l++) {
+            for (int m = 0; m < 4; m++) {
+                System.out.println(floor4[l][m]);
+            }
+        }
+
+    }
+
+    //Comparamos los caracteres de nuestra contrasena con el que contiene todos los faltantes
+    //para removerlos y tener una lista mas limpia.
+    public static ArrayList<String> preLlenado (ArrayList<String> passwd) {
+        for (int i= 0 ; i < passwd.size();i++) {
             for (int j=0; j< listpass.size(); j++){
-                if (password.get(i).equals(listpass.get(j))){
+                if (passwd.get(i).equals(listpass.get(j))){
                     listpass.remove(j);
                 }
             }
@@ -40,50 +122,97 @@ public class Main {
         return listpass;
     }
 
+    //En esta funcion se crean lo trigramas del mensaje introducido
+    //Estos se almacenan en la matriz "trigramas"
+    public static String[][] crearTrigramas(String message) {
+        int j = 0;
+        String[][] trigramas = new String[message.length()/2][3];
+        for (int i=0; i < message.length(); i = i+3) {
+            if (message.charAt(i) != message.charAt(i+1) && message.charAt(i+1) != message.charAt(i+2)) {
+                trigramas[j][0] = String.valueOf(message.charAt(i));
+                trigramas[j][1] = String.valueOf(message.charAt(i+1));
+                trigramas[j][2] = String.valueOf(message.charAt(i+2));
+                j++;
+            }
+            else if (String.valueOf(message.charAt(i)) != String.valueOf(message.charAt(i+1))) {
+                trigramas[j][0] = String.valueOf(message.charAt(i));
+                trigramas[j][1] = String.valueOf(message.charAt(i+1));
+                trigramas[j][2] = "X";
+                i--;
+                j++;
+            }
+            else {
+                trigramas[j][0] = String.valueOf(message.charAt(i));
+                trigramas[j][1] = "X";
+                trigramas[j][2] = String.valueOf(message.charAt(i+1));
+                i--;
+                j++;
+            }
+        }
+        for (int l = 0; l < (message.length()/2) +1; l++) {
+            for (int m = 0; m < 3; m++) {
+                System.out.println(trigramas[l][m]);
+            }
+        }
+        return trigramas;
+    }
 
     public static void main(String[] args) {
+
+        //Obtenemos todos los caracteres necesarios con base al codigo ascii
+
+        //LETRAS
         for(int i=48; i < 58; i++) {
             char  letter = (char) i ;
             String letter2 = String.valueOf(letter);
             listpass.add(letter2);
         }
 
+        //NUMEROS
         for(int i=65; i < 91; i++) {
-            char  letter = (char) i ;
-            String letter2 = String.valueOf(letter);
-            listpass.add(letter2);
+            char  number = (char) i ;
+            String number2 = String.valueOf(number);
+            listpass.add(number2);
         }
 
+        //SIMBOLOS
         for(int i=33; i < 125; i++) {
             if (i >= 33 && i < 48) {
-                char  letter = (char) i ;
-                String letter2 = String.valueOf(letter);
-                listpass.add(letter2);
+                char  simbols = (char) i ;
+                String simbols2 = String.valueOf(simbols);
+                listpass.add(simbols2);
             }
             if (i >= 58 && i < 65) {
-                char  letter = (char) i ;
-                String letter2 = String.valueOf(letter);
-                listpass.add(letter2);
+                char  simbols = (char) i ;
+                String simbols2 = String.valueOf(simbols);
+                listpass.add(simbols2);
             }
             if (i >= 91 && i < 96) {
-                char  letter = (char) i ;
-                String letter2 = String.valueOf(letter);
-                listpass.add(letter2);
+                char  simbols = (char) i ;
+                String simbols2 = String.valueOf(simbols);
+                listpass.add(simbols2);
             }
             if (i==124) {
-                char  letter = (char) i ;
-                String letter2 = String.valueOf(letter);
-                listpass.add(letter2);
+                char  simbols = (char) i ;
+                String simbols2 = String.valueOf(simbols);
+                listpass.add(simbols2);
             }
         }
 
-        System.out.println(listpass.size());
-
-        System.out.println("Ingrese una contrasena");
         Scanner scanner = new Scanner(System.in);
-        String password = scanner.nextLine().toUpperCase();
-        preLlenado(checkPass(password));
-        //llenarMatriz(password);
 
+        //Leemos del usuario el mensaje que desea encriptar
+        System.out.println("Ingrese el mensaje que desea encriptar");
+        String message = scanner.nextLine().toUpperCase();
+
+        //Leemos la contrasena con la que podra acceder al mensaje
+        System.out.println("Ingrese una contrasena");
+        String password = scanner.nextLine().toUpperCase();
+
+
+        ArrayList<String> passwd = checkPass(password);
+        listpass =(preLlenado(passwd));
+        llenarMatrices(passwd);
+        crearTrigramas(message);
     }
 }
